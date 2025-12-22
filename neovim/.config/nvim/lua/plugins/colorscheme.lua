d09e2e1e24f8f7c0b42af0a7e3f138cdc734a49c
masisz <masisz.1567@gmail.com>
2025-12-22 19:05:09 +0900

    The default color thmee is changed to Wisteria.

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,21 @@
 return {
   {
     "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "wisteria",
+    }
   },
+  {
+    "masisz/wisteria.nvim",
+    name = "wisteria",
+    opts = {
+      transparent = true,
+    },
+    {
+      "nvim-lualine/lualine.nvim",
+      opts = {
+        theme = "wisteria",
+      },
+    }
+  }
 }
